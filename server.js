e52d957153a59e2a19050c79993f16974805990b
tskorupka <tomaszskorupka94@gmail.com>
2016-01-28 00:00:06 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var moment = require("moment");
+var app = express();
+
+app.get('/:time', function (request, response) {
+    var rawTime = request.params.time;
+    var date;
+    
+    if ( Number.isInteger(parseInt(rawTime)) ) {
+        date = new Date(rawTime*1000); 
+    } else {
+        date = new Date(rawTime);
+    }
+    
+    response.send(JSON.stringify({
+        unix: date.getTime() / 1000,
+        natural: moment(date).format('MMMM D, YYYY') === 'Invalid date' ? null : moment(date).format('MMMM D, YYYY')
+    }));
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
+    console.log('listening');
+});
